170f9cebe4239bd3bf1c5e1f7e5ffa09136cfeb8
mrngsht <mrngsht@gmail.com>
2024-10-09 19:12:34 +0900

    update

--- src/microcms.ts
+++ src/microcms.ts
@@ -1,50 +1,68 @@
-import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 import { preloadImage } from "./preload_image";
+import { JSDOM } from "jsdom"
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
-//型定義
-export type NewsPost = {
-  id: string;
-  publishedAt: string;
-  title: string;
-  thumbnail: {
-    url: string;
-    height: number;
-    width: number;
-  };
-  content: string;
-};
 export type NewsResponse = {
   totalCount: number;
   offset: number;
   limit: number;
-  contents: NewsPost[];
+  contents: NewsPostSummary[];
 };
 
-export const getNewsPosts = async (queries?: MicroCMSQueries): Promise<NewsResponse> => {
-  const res = await client.get<NewsResponse>({ endpoint: "news", queries });
+export type NewsPostSummary = {
+  id: string;
+  publishedAt: string;
+  title: string;
+  thumbnail: CmsImage;
+};
+
+export const getNewsPosts = async (): Promise<NewsResponse> => {
+  const res = await client.get<NewsResponse>({ 
+    endpoint: "news", 
+    queries: {fields: ["id", "publishedAt", "title", "thumbnail"]},
+  });
   await Promise.all(res.contents.map(preloadAndReplaceImageUrl))
   return res
 }
 
-export const getNewsPost = async (
-  contentId: string,
-  queries?: MicroCMSQueries
-): Promise<NewsPost> => {
-  const res = await client.getListDetail<NewsPost>({
+export type NewsPostDetail = {
+  id: string;
+  publishedAt: string;
+  title: string;
+  thumbnail: CmsImage;
+  content: string;
+};
+
+export const getNewsPost = async (contentId: string): Promise<NewsPostDetail> => {
+  const res = await client.getListDetail<NewsPostDetail>({
     endpoint: "news",
     contentId,
-    queries,
+    queries: {fields: ["id", "publishedAt", "title", "thumbnail", "content"]},
   });
   await preloadAndReplaceImageUrl(res)
+  const dom = new JSDOM(res.content)
+  for (const x of dom.window.document.querySelectorAll("img")) {
+     const imgsrc = x.getAttribute("src")
+     if (imgsrc) {
+       x.setAttribute("src",  await preloadImage(imgsrc))
+     }
+  }
+  res.content = dom.serialize()
   return res
 };
 
-const preloadAndReplaceImageUrl = async (post: NewsPost) => {
+const preloadAndReplaceImageUrl = async (post: {thumbnail: CmsImage}) => {
   post.thumbnail.url = await preloadImage(post.thumbnail.url)
 }
+
+export type CmsImage = {
+  url: string;
+  height: number;
+  width: number;
+}
+
