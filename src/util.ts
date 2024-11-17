267d4c730d2f8294a6b77cc98cdc34dc93801fdc
mrngsht <mrngsht@gmail.com>
2024-11-17 12:23:59 +0900

    complete ogp

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom"
+
 export const formatDate = (d: Date): string => {
   const year = d.getFullYear()
   const month = d.getMonth()
@@ -5,3 +7,10 @@ export const formatDate = (d: Date): string => {
   return `${year}.${month}.${date}`
 };
 
+export const getInnerTextWithEllipsis = (html: string): string => {
+  const maxLength = 80;
+  const jsdom = new JSDOM();
+  const parser = new jsdom.window.DOMParser();
+  const fullText = parser.parseFromString(html, "text/html").body.textContent?.trim() ?? "";
+  return fullText.length <= maxLength ? fullText : `${fullText.substring(0, maxLength)}...`;
+}
